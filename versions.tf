913183f571c1e05741d9053b5528d099136a492c
Brad Sickles <brad.sickles@gmail.com>
2026-06-18 13:51:20 -0400

    upgraded tf providers

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ns = {
       source  = "nullstone-io/ns"
-      version = "~> 0.10"
+      version = "~> 0.11"
     }
     aws = {
       source  = "hashicorp/aws"
